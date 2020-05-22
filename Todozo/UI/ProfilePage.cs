cdc6c235f946a23cf4561e5d684598f5d09d5cbb
Emil Ravn <emilravn@outlook.com>
2020-05-22 13:05:12 +0200

    initial delete user

---
+++ Todozo/UI/ProfilePage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Todozo.UI
+{
+    public partial class ProfilePage : Form
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private void ProfilePage_Load(object sender, EventArgs e)
+        {
+            UserName.Text = $@"Hello {UserLoginPage.activeUser.Name}!";
+        }
+
+        private void btnDeleteUser_Click(object sender, EventArgs e)
+        {
+            var confirmDelete = MessageBox.Show(@"Are you sure you want to delete this user along with all of data?",
+                @"Confirm",
+                MessageBoxButtons.YesNo);
+            if (confirmDelete == DialogResult.Yes)
+            {
+                // Deletes the user and all of its content.
+                DataAccess db = new DataAccess();
+                db.DeleteUser(UserLoginPage.activeUser.Name);
+            }
+            else
+            {
+                Close();
+            }
+        }
+    }
+}
